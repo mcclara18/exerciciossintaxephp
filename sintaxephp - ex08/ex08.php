fe0e0ce2edb100ef46b1f7aef0bf8c253571dcb6
Maria Clara Landim <141859035+mcclara18@users.noreply.github.com>
2025-05-27 14:01:11 -0300

    Add files via upload

---
+++ sintaxephp - ex08/ex08.php
@@ -0,0 +1,4 @@
+$categoria = "0092147852Mc";
+echo "<p>Categoria antiga: $categoria</p>";
+$categoria = 147852;
+echo "<p>Nova categoria: $categoria</p>";
---
+++ sintaxephp - ex08/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exercício 08 - Mudança de Categoria</title>
+</head>
+<body>
+    <?php
+$categoria = "0092147852Mc";
+echo "<p>Categoria antiga: $categoria</p>";
+$categoria = 147852;
+echo "<p>Nova categoria: $categoria</p>";
+    ?>
+</body>
+</html>
