e2786f30599b88fc5675d5206f8a0a69f02810fc
Maria Clara Landim <141859035+mcclara18@users.noreply.github.com>
2025-05-27 13:49:09 -0300

    Add files via upload

---
+++ sintaxephp - ex03/ex03.php
@@ -0,0 +1,2 @@
+$areaterreno = 150.00;
+echo "<p>Área do terreno: {$areaterreno} m²</p>";
---
+++ sintaxephp - ex03/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ex03 - Medição de Terreno</title>
+</head>
+<body>
+    <?php
+    $areaterreno = 150.00;
+    echo "<p>Área do terreno: {$areaterreno} m²</p>";
+    ?>
+</body>
+</html>
