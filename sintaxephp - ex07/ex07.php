455c8224395a1309f407181c678840c15daa7333
Maria Clara Landim <141859035+mcclara18@users.noreply.github.com>
2025-05-27 13:57:07 -0300

    Add files via upload

---
+++ sintaxephp - ex07/ex07.php
@@ -0,0 +1,3 @@
+$rua = "Prado";
+$numero = 19;
+echo "<p>Endereço de entrega: $rua, Nº $numero</p>";
---
+++ sintaxephp - ex07/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ex07 - Endereço</title>
+</head>
+<body>
+    <?php
+   $rua = "Prado";
+   $numero = 19;
+   echo "<p>Endereço de entrega: $rua, Nº $numero</p>";
+    ?>
+</body>
+</html>
